b87b05939747af108dc49f6d505d99dec46f496d
Lucas <juangutierrezutnfrt@gmail.com>
2023-07-21 12:56:52 -0300

    Integrador M3

---
+++ Server/src/routes/character.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const { getCharById } = require("../controllers/getCharById");
+
+const characterRouter = express.Router();
+
+
+
+characterRouter.get("/:idChar", getCharById);
+
+module.exports = characterRouter;
+
+
+
